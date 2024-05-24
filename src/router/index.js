3a20ceae32fe1b32f217316d9d9320dbae576af8
NexGenLukeet <lukeet333@gmail.com>
2024-05-24 10:44:27 +0800

    简单建立了登录注册和主页的路由，那么从这里开始，新建feature-account 和 feature-mainpage分支，

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,31 @@
 import { createMemoryHistory, createRouter } from 'vue-router'
 
+// 主页一级路由
+import home from '../view/home.vue';
+// 对应的二级路由；
+import mainpage from '../view/homeview/mainPageView.vue'
 
-import home from '../view/home.vue'
+// 账户的登录和注册
+import account from '../view/accountView.vue';
+import loginView from '../view/accountView/loginView.vue';
+import signupView from '../view/accountView/signupView.vue';
 
 const routes = [
-  { path: '/', component: home },
-  
+  { path: '/', redirect: '/home/mainpage' },
+  {
+    path: '/home', component: home,
+    children: [
+      { path: 'mainpage', component: mainpage }
+    ]
+  },
+  {
+    path: '/account', component: account,
+    children: [
+      { path: '/login', component: loginView },
+      { path: '/signup', component: signupView }
+    ]
+  }
+
 ]
 
 const router = createRouter({
--- src/view/home.vue
+++ src/view/home.vue
@@ -3,5 +3,34 @@
 </script>
 
 <template>
-    <div>你好</div>
+    <div class="homecontainer">
+        <div class="homechildrencontainer">
+            <RouterView />
+        </div>
+        <div class="bottomnav">
+            <div>首页</div>
+            <div>首页</div>
+            <div>首页</div>
+            <div>首页</div>
+            <div>首页</div>
+        </div>
+    </div>
 </template>
+
+<style>
+.homecontainer{
+    width:100vw;
+    height:100vh;
+}
+.homechildrencontainer{
+    width:100vw;
+    height:calc(100vh - 50px);
+}
+.bottomnav{
+    width:100vw;
+    height: 50px;
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+}
+</style>
